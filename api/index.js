3399c727ed6a36297007ab1a24cbcf679f530a81
georgetreger <georgetreger@gmail.com>
2024-02-18 04:35:15 -0800

    created a middleware

--- api/index.js
+++ api/index.js
@@ -2,16 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
-import authRouter from './routes/auth.route.js';
-
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
   console.log("Connect to MongoDB");
 });
-   const app = express();
-   
-   app.use(express.json());
+const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 300!!!!!");
@@ -19,3 +18,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
