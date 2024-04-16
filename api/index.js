eb1021dffc0a18407c1e5f5868e39fade128ffc8
georgetreger <georgetreger@gmail.com>
2024-04-16 04:54:10 -0700

    create middleware and function to handle possible errs

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import bcrypt from 'bcryptjs'
+
 import dotenv from 'dotenv'
 import userRouter from "./routes/user.route.js";
 import authRouter from './routes/auth.route.js'
@@ -21,9 +21,17 @@ mongoose.connect(process.env.MONGO).then(()=>{
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 
-
+app.use((err, req, res, next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+}) 
 app.listen(4000, () => {
-  console.log("Server is running on port 4000!!");
+  console.log("Server is running on port 4000!");
 });
 
 
