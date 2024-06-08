3e30db351bee7a36890d7699411208d15539315c
georgetreger <georgetreger@gmail.com>
2024-06-08 07:27:05 -0700

    create search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import CreateListing from "./pages/CreateListing";
 
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
             <Route path="/sign-in" element={<SignIn />} />
             <Route path="/sign-up" element={<SignUp />} />
             <Route path="/about" element={<About />} />
+             <Route path="/search" element={<Search/>}/>
             <Route path="/listing/:listingId" element={<Listing />} />
             <Route element={<PrivateRoute />}>
               <Route path="/profile" element={<Profile />} />
