7e7dd47189b9ca4056a6e0f24e3904f5ff918ce8
georgetreger <georgetreger@gmail.com>
2024-06-09 08:26:48 -0700

    add show listing functionality to the page

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { MdLocationOn } from "react-icons/md";
 
-export default function ListingItem() {
+export default function ListingItem({ listing }) {
   return (
-    <div>ListingItem</div>
-  )
+    <div className="bg-white shadow-md hover:shadow-lg overflow-hidden transition-shadow rounded-lg w-full sm:w-[270px]">
+      <Link to={`/listing/${listing._id}`}>
+        <img
+          src={
+            listing.imageUrls[0] ||
+            "https://img.indiafilings.com/learn/wp-content/uploads/2015/10/12011006/Real-Estate-Agent-Business-India.jpg"
+          }
+          alt="listing"
+          className="h-[320px] sm:-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
+        />
+        <div className="p-3 flex flex-col gap-2 w-full">
+          <p
+            className="text-lg font-semibold text-slate-700 truncate 
+           w-full
+          "
+          >
+            {listing.name}
+          </p>
+          <div className="flex items-center gap-2">
+            <MdLocationOn className="h-4 w-4 text-green-700" />
+            <p className="text-gray-700 truncate text-sm w-full">
+              {listing.address}
+            </p>
+          </div>
+          <p className="text-sm text-gray-700 line-clamp-2">
+            {listing.description}
+          </p>
+          <p className="text-slate-600 mt-2 font-semibold">
+            $
+            {listing.offer
+              ? listing.discou8ntPrice.toLocaleString("en-US")
+              : listing.regularPrice.toLocaleString("en-US")}
+            {listing.type === "rent" && "/month"}
+          </p>
+          <div className="text-slate-700 flex gap-2">
+            <div className="font-bold text-xs">
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} beds`
+                : `${listing.bedrooms} bed`}
+            </div>
+            <div className="font-bold text-xs">
+              {listing.bedrooms > 1
+                ? `${listing.bathrooms} baths`
+                : `${listing.bathrooms} bath`}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 }
