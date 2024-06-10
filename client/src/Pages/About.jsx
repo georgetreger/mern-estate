3651cc81bc49cf396d0a027ea894a19c4f43cc9d
georgetreger <georgetreger@gmail.com>
2024-06-10 03:21:00 -0700

    complete about page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold text-slate-800 mb-3'>About kwame estate</h1>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro labore
+        sint incidunt, nulla, explicabo quia ipsa, neque rem reprehenderit
+        quibusdam iusto quod. Quae minus odio laboriosam asperiores consectetur,
+        atque ipsum.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, iure?
+        Deleniti quos, mollitia autem, tempore consequuntur, cupiditate ratione
+        explicabo dolorum quaerat aliquid consequatur soluta iure quisquam
+        obcaecati laboriosam voluptatum! Porro.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, iure?
+        Deleniti quos, mollitia autem, tempore consequuntur, cupiditate ratione
+        explicabo dolorum quaerat aliquid consequatur soluta iure quisquam
+        obcaecati laboriosam voluptatum! Porro.
+      </p>
     </div>
-  )
+  );
 }
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -48,12 +48,12 @@ export default function Header() {
         </form>
         <ul className="flex gap-4">
           <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden sm:inline text-white hover:underline">
               Home
             </li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden sm:inline text-white hover:underline">
               About
             </li>
           </Link>
