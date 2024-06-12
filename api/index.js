2b328d44096b6622777912de7f33d0dc8abc86d8
georgetreger <georgetreger@gmail.com>
2024-06-12 11:22:09 -0700

    some touches

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const app = express();
 dotenv.config();
 //   middlewares
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser())
 app.use(cors());
 
 app.use("/api/user", userRoute);
--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
-      <h1 className='text-3xl font-bold text-slate-800 mb-3'>About kwame estate</h1>
+      <h1 className='text-3xl font-bold text-slate-800 mb-3'>About kwame Estate</h1>
       <p className='mb-4 text-slate-700'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro labore
         sint incidunt, nulla, explicabo quia ipsa, neque rem reprehenderit
