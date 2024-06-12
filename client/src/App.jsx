edf911a4816a2bf19617b6416ea37809d90ee2c2
georgetreger <georgetreger@gmail.com>
2024-06-12 15:19:40 -0700

    some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
-import About from "./pages/About";
+import About from './pages/About';
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
