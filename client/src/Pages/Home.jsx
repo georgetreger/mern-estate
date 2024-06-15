c45bd991d1438ecedb511db6193d1279e01883df
georgetreger <georgetreger@gmail.com>
2024-06-15 13:10:45 -0700

    some change

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,3 +1,158 @@
+// import { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Navigation } from "swiper/modules";
+// import SwiperCore from "swiper";
+// import "swiper/css/bundle";
+// import ListingItem from "../components/ListingItem";
+
+// export default function Home() {
+//   const [offerListings, setOfferListings] = useState([]);
+//   const [saleListings, setSaleListings] = useState([]);
+//   const [rentListings, setRentListings] = useState([]);
+//   SwiperCore.use([Navigation]);
+
+//   useEffect(() => {
+//     const fetchOfferListings = async () => {
+//       try {
+//         const res = await fetch("/api/listing/get?offer=true&limit=4");
+//         const data = await res.json();
+//         setOfferListings(data);
+//         fetchRentListings();
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+//     const fetchRentListings = async () => {
+//       try {
+//         const res = await fetch("/api/listing/get?type=rent&limit=4");
+//         const data = await res.json();
+//         setRentListings(data);
+//         fetchSaleListings();
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+
+//     const fetchSaleListings = async () => {
+//       try {
+//         const res = await fetch("/api/listing/get?type=sale&limit=4");
+//         const data = await res.json();
+//         setSaleListings(data);
+//       } catch (error) {
+//         log(error);
+//       }
+//     };
+//     fetchOfferListings();
+//   }, []);
+//   return (
+//     <div className="p-3">
+//       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+//         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+//           Find your next <span className="text-slate-500">perfect</span>
+//           <br />
+//           place with ease
+//         </h1>
+//         <div className="text-gray-400 text-xs sm:text-sm">
+//           Kwame Estate is the best place to find your next perfect place to
+//           live.
+//           <br />
+//           We have a wide range of properties for you to choose from.
+//         </div>
+//         <Link
+//           to={"/search"}
+//           className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+//         >
+//           Let's start now...
+//         </Link>
+//       </div>
+
+//       <Swiper navigation>
+//         {offerListings &&
+//           offerListings.length > 0 &&
+//           offerListings.map((listing) => (
+//             <SwiperSlide key={listing._id}>
+//               <div
+//                 key={listing._id}
+//                 style={{
+//                   background: `url(${listing.imageUrls[0]}) center no-repeat`,
+//                   backgroundSize: "cover",
+//                 }}
+//                 className="h-[500px]"
+//               ></div>
+//             </SwiperSlide>
+//           ))}
+//       </Swiper>
+
+//       {/* listing results for offer, sale and rent */}
+
+//       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+//         {offerListings && offerListings.length > 0 && (
+//           <div className="p-3">
+//             <div className="my-3">
+//               <h2 className="text-2xl font-semibold text-slate-600">
+//                 Recent offers
+//               </h2>
+//               <Link
+//                 className="text-sm text-blue-800 hover:underline"
+//                 to={"/search?offer=true"}
+//               >
+//                 Show more offers
+//               </Link>
+//             </div>
+//             <div className="flex flex-wrap gap-4">
+//               {offerListings.map((listing) => (
+//                 <ListingItem listing={listing} key={listing._id} />
+//               ))}
+//             </div>
+//           </div>
+//         )}
+//         {rentListings && rentListings.length > 0 && (
+//           <div className="">
+//             <div className="my-3">
+//               <h2 className="text-2xl font-semibold text-slate-600">
+//                 Recent places for rent
+//               </h2>
+//               <Link
+//                 className="text-sm text-blue-800 hover:underline"
+//                 to={"/search?type=rent"}
+//               >
+//                 Show more places for rent
+//               </Link>
+//             </div>
+//             <div className="flex flex-wrap gap-4">
+//               {rentListings.map((listing) => (
+//                 <ListingItem listing={listing} key={listing._id} />
+//               ))}
+//             </div>
+//           </div>
+//         )}
+//         {saleListings && saleListings.length > 0 && (
+//           <div className="p-3">
+//             <div className="my-3">
+//               <h2 className="text-2xl font-semibold text-slate-600">
+//                 Recent places for sale
+//               </h2>
+//               <Link
+//                 className="text-sm text-blue-800 hover:underline"
+//                 to={"/search?type=sale"}
+//               >
+//                 Show more places for sale
+//               </Link>
+//             </div>
+//             <div className="flex flex-wrap gap-4">
+//               {saleListings.map((listing) => (
+//                 <ListingItem listing={listing} key={listing._id} />
+//               ))}
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -11,7 +166,7 @@ export default function Home() {
   const [saleListings, setSaleListings] = useState([]);
   const [rentListings, setRentListings] = useState([]);
   SwiperCore.use([Navigation]);
-
+  console.log(offerListings);
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
@@ -46,7 +201,8 @@ export default function Home() {
     fetchOfferListings();
   }, []);
   return (
-    <div className="p-3">
+    <div>
+      {/* top */}
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
           Find your next <span className="text-slate-500">perfect</span>
@@ -54,7 +210,7 @@ export default function Home() {
           place with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Kwame Estate is the best place to find your next perfect place to
+          Sahand Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
@@ -63,22 +219,23 @@ export default function Home() {
           to={"/search"}
           className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
         >
-          Let's start now...
+          Let's get started...
         </Link>
       </div>
 
+      {/* swiper */}
       <Swiper navigation>
         {offerListings &&
           offerListings.length > 0 &&
           offerListings.map((listing) => (
-            <SwiperSlide key={listing._id}>
+            <SwiperSlide>
               <div
-                key={listing._id}
                 style={{
                   background: `url(${listing.imageUrls[0]}) center no-repeat`,
                   backgroundSize: "cover",
                 }}
                 className="h-[500px]"
+                key={listing._id}
               ></div>
             </SwiperSlide>
           ))}
@@ -88,7 +245,7 @@ export default function Home() {
 
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
         {offerListings && offerListings.length > 0 && (
-          <div className="p-3">
+          <div className="">
             <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
                 Recent offers
@@ -128,7 +285,7 @@ export default function Home() {
           </div>
         )}
         {saleListings && saleListings.length > 0 && (
-          <div className="p-3">
+          <div className="">
             <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
                 Recent places for sale
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   server:{
-    proxy:{
+  plugins: [react()],
+  server: {
+    proxy: {
       "/api": {
-       target: "http://localhost:5000",
-       secure: false,
+        target: "http://localhost:5000",
+        secure: false,
       },
     },
-   },
-  plugins: [react()],
+  },
 });
